2b7490bbd0170b285ef780b2bbb1748a5b5cd0e4
Vinicius Santos <viniciusmsos@gmail.com>
2025-12-18 20:36:31 -0300

    2025 day 4

---
+++ 25/Day04B.cs
@@ -0,0 +1,43 @@
+var map = File.ReadAllLines("input/day04.txt").Select(l => l.AsEnumerable().ToArray()).ToArray();
+
+var rows = map.Length;
+var cols = map[0].Length;
+List<(int X, int Y)> directions =
+[
+    (-1, -1),
+    (-1, 0),
+    (-1, 1),
+    (0, -1),
+    (0, 1),
+    (1, -1),
+    (1, 0),
+    (1, 1),
+];
+
+var count = 0;
+
+for (var x = 0; x < rows; x++)
+for (var y = 0; y < cols; y++)
+{
+    var c = map[x][y];
+
+    if (c is not '@')
+        continue;
+
+    var adjacent = directions.Count(d =>
+        x + d.X >= 0
+        && x + d.X < rows
+        && y + d.Y >= 0
+        && y + d.Y < cols
+        && map[x + d.X][y + d.Y] == '@'
+    );
+
+    if (adjacent < 4)
+    {
+        map[x][y] = 'x';
+        x = y = 0;
+        count++;
+    }
+}
+
+Console.WriteLine($"{count}");
